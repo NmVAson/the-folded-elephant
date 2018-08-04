72cce90e7c6238b93a92ae43d9b405ec7e4e61ef
NmVAson <giulswatson@gmail.com>
2018-08-04 16:32:16 -0400

    Make failing test pass

--- test/landing.spec.js
+++ test/landing.spec.js
@@ -1,7 +1,10 @@
+var assert = require('chai').assert,
+    expect = require('chai').expect;
+
 describe("A test suite", function() {
     beforeEach(function() { });
 
     afterEach(function() { });
-    
-    it('should fail', function() { expect(true).to.be.false; });
+
+    it('should pass', function() { expect(true).to.be.true; });
  });
