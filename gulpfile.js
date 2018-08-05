66e435bade9ac1f013d04e3c69871e1bd5ffce7f
NmVAson <giulswatson@gmail.com>
2018-08-05 00:19:26 -0400

    Compile and run tests on npm start

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify = require('gulp-minify');
+const mocha = require('gulp-mocha');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
- 
 gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
  
-gulp.task('compress', function() {
-  gulp.src(['./src/js/*.js'])
+gulp.task('js', function() {
+    gulp.src(['./src/js/*.js'])
     .pipe(minify({
         noSource: true
     }))
     .pipe(gulp.dest('./public/dist'))
 });
+
+gulp.task('test', function() {
+    gulp.src(['./test/*.spec.js'], {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
+});
+
+gulp.task('default', [ 'sass','js','test']);
